3a6353942ffc1bd21bd57bc8a0b967cc7de23892
Alexandre Froger <alex@froger.me>
2018-05-13 15:54:52 +0800

    fix query

--- uninstall.php
+++ uninstall.php
@@ -6,8 +6,8 @@ if ( ! defined( 'ABSPATH' ) || ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
 
 global $wpdb;
 
-$option_prefix    = $wpdb->esc_like( '_transient_wp_weixin_' );
-$transient_prefix = $wpdb->esc_like( 'wp_weixin_' );
+$option_prefix    = $wpdb->esc_like( '_transient_wp_weixin_' ) . '%';
+$transient_prefix = $wpdb->esc_like( 'wp_weixin_' ) . '%';
 $sql              = "DELETE FROM $wpdb->options WHERE `option_name` LIKE '%s' OR `option_name` LIKE '%s'";
 
 $wpdb->query( $wpdb->prepare( $sql, $option_prefix . '%', $transient_prefix . '%' ) );// @codingStandardsIgnoreLine
